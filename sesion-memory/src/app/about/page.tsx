da328e47d148b22dcf9c15d61b06e31a7fa499fd
didacfrndz <156692497+didacfrndz@users.noreply.github.com>
2025-04-08 17:33:37 +0000

    primercommit

---
+++ sesion-memory/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    return (
+        <div>
+          <h1>Hola Next</h1>
+          <h2>Pagina About</h2>
+        </div>
+      );
+}
---
+++ sesion-memory/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hola Next</h1>
+    </div>
+  );
+}
